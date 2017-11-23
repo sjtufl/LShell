8ccd732f56899fa8104fc8e447a6f5d504c43dcc
sjtufl <jerryfang1995@qq.com>
2017-11-23 22:14:32 +0800

    initial

---
+++ prefix.c
@@ -0,0 +1,15 @@
+//
+// Created by fl on 17-11-13.
+//
+#include "lshell.h"
+
+const int max_hostname_len = 256;
+const int max_username_len = 1024;
+
+void prefix(char *prompt)
+{
+  extern struct passwd *pwd;
+
+  int length;
+
+}
